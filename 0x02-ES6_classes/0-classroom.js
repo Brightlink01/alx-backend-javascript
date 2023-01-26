eb836144a1a00faca2c09faae10da9790db037ac
Tsedal Lombebo <Brightlink01@gmail.com>
2023-01-26 18:16:34 +0000

    commit

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,5 @@
 export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    // eslint-disable-next-line no-underscore-dangle
-      this._maxStudentsSize = maxStudentsSize;
+    constructor(maxStudentsSize) {
+  this._maxStudentsSize = maxStudentsSize;
     }
 }
-
