29e1c79ca1ed99e944dca988c13413784542b9d6
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-01-27 12:54:51 -0800

    fixing

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
-export default function guardrail(mathFunction) {
-    const queue = [];
+export default function guardrail (mathFunction) {
+  const queue = []
 
-    try {
-	queue.push(mathFunction());
-    } catch (error) {
-	queue.push(`Error: ${error.message}`);
-    }
-    queue.push('Guardrail was processed');
+  try {
+    queue.push(mathFunction())
+  } catch (error) {
+    queue.push(`Error: ${error.message}`)
+  }
+  queue.push('Guardrail was processed')
 
-    return queue;
+  return queue
 }
