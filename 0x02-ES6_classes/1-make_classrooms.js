4215c4cb2d68ebc7ce66e310ef2e4fee5961258d
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-01-28 10:58:43 -0800

    fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms () {
+export default function initializeRooms(){
   const clsRooms = [];
   clsRooms.push(new ClassRoom(19));
   clsRooms.push(new ClassRoom(20));
