75ad99fe86310bec71cef350d04f3c90d3543de7
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-01-28 12:49:02 -0800

    qcommit

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-param-reassign */
-export default function appendToEachArrayValue (array, appendString) {
-  const newArr = []
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
   for (const idx of array) {
-    newArr.push(appendString + idx)
+    newArr.push(appendString + idx);
   }
 
-  return newArr
+  return newArr;
 }
