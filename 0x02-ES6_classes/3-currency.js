c73c902a212703f053c62e60c93e4d237ef7601a
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-01-28 11:30:20 -0800

    fix

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,28 @@
 /* eslint-disable no-underscore-dangle */
 class Currency {
-    constructor(code = '', name = '') {
-	this._code = code;
-	this._name = name;
-    }
+  constructor(code = '', name = '') {
+    this._code = code;
+    this._name = name;
+  }
 
-    set code(code = '') {
-	this._code = code;
-    }
+  set code(code = '') {
+    this._code = code;
+  }
 
-    get code() {
-	return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set name(name = '') {
-	this._name = name;
-    }
+  set name(name = '') {
+    this._name = name;
+  }
 
-    get name() {
-	return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    displayFullCurrency() {
-	return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
 export default Currency;
