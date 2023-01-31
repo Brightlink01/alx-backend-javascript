191cd98dab62da878ca6fbe7159d87da2fd15f8f
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-01-31 11:47:56 -0800

    fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,13 @@
-/**
- * @param {string | any[]} students
- */
-export default function getListStudentIds(students) {
-  if (Array.isArray(students)) {
-    return students.map((items) => items.id);
+/* eslint-disable no-unused-vars */
+export default function getListStudentIds(obj) {
+  const myArr = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  const mappedArr = myArr.map((element) => element.id);
+  if (Array.isArray(obj)) {
+    return mappedArr;
   }
   return [];
 }
