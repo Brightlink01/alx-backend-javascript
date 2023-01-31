4688acb512f367346be5207ce41b03e27e0b594d
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-01-31 12:18:08 -0800

    fixing

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,7 @@
 export default function updateUniqueItems(groceriesMap) {
   if (!(groceriesMap instanceof Map)) throw new Error('Cannot process');
-    groceriesMap.forEach((value, key) => {
-      if (value === 1) groceriesMap.set(key, 100);
+  groceriesMap.forEach((value, key) => {
+    if (value === 1) groceriesMap.set(key, 100);
   });
   return groceriesMap;
 }
-
--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,5 +3,5 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
   if (count >= 4) throw new Error('Endpoint load is high');
-    weakMap.set(endpoint, count + 1);
+  weakMap.set(endpoint, count + 1);
 }
